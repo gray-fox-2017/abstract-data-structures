c39c84dbc9f09f672939859582c37e5faf732e03
Zulfikar Annur Ahmad <zlfikar.aa@gmail.com>
2017-04-27 19:49:41 +0700

    basic crud + search by value

--- abstract_data_structures.js
+++ abstract_data_structures.js
@@ -1,7 +1,35 @@
+//src = https://en.wikipedia.org/wiki/Associative_array
 // 1. Describe
-
+/*
+Associative array, atau dalam JS disebut dengan Object, dan memiliki Class sebagai template dalam create object baru. Merupakan kumpulan dari pasangan "key:value" yang mendukung fungsi CRUD.
+*/
 // ------------
 // 2. Implement
-
+// Create Object
+let libraryLoan = new Object();
+libraryLoan["Bumi Manusia"] = "Zulfikar";
+libraryLoan["Kumpulan Puisi Angkatan 66"] = "Bambang";
+libraryLoan["Arok Dedes"] = "Bambang";
+libraryLoan["Di Bawah Bendera Revolusi"] = "Joko"
+console.log(libraryLoan)
+console.log("------------------------------");
+console.log("Search Value by Key");
+console.log(libraryLoan["Bumi Manusia"]);
+console.log(libraryLoan["Arok Dedes"]);
+console.log("-------------------------------");
+console.log("Search Key by Value, Not Found");
+console.log("Data size");
+console.log(Object.keys(libraryLoan).length);
+console.log("-------------------------------");
+console.log("Update Data");
+libraryLoan["Arok Dedes"] = "Mulyadi";
+console.log(libraryLoan);
+console.log("-------------------------------");
+console.log("Delete");
+delete libraryLoan["Arok Dedes"];
+console.log(libraryLoan);
 // ------------
 // 3. Get real
+/*
+Menurut referensi, model data di atas digunakan untuk menyimpan hash table yang digunakan untuk memberikan index ke suatu property dengan pengelompokan "bucket"
+*/
